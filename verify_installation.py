2a8ed9b480997ed3a059c510210f08003821339d
Vijay Joshi <joshivijay2004@gmail.com>
2025-12-22 12:31:47 +0530

    Removed unnecessary files

--- verify_installation.py
+++
@@ -1,9 +0,0 @@
-try:
-    import cv2
-    import mediapipe
-    import pygame
-    import numpy
-    import scipy
-    print("All dependencies installed successfully.")
-except ImportError as e:
-    print(f"Import Error: {e}")
